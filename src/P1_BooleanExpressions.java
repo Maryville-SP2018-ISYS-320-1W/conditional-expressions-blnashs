843efca6682519b0992cffa43be16c3a57b805e7
Barbara Nash <bnash1@live.maryville.edu>
2018-04-22 10:28:31 -0500

    string

--- src/P1_BooleanExpressions.java
+++ src/P1_BooleanExpressions.java
@@ -7,30 +7,29 @@
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
- 
- 5.
- 6.
- 7.
- 8.
+ 1. not false
+ 2. false or true
+ 3. 32 > -1 and -1 > 32
+ 4. false
+ 5. false
+ 6. false
+ 7. true
+ 8. true
    
  */
 
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
+ 1. I put not false, should have been true
+ 2. No should have been true
+ 3.  No false
+ 4. no, I put false should have been true as 27 is less than 32
  
- 5.
- 6.
- 7.
- 8.
+ 5. I had right but second guessed
+ 6. correct
+ 7. correct
+ 8. correct
 
  */
 public class P1_BooleanExpressions {
@@ -41,7 +40,7 @@ public class P1_BooleanExpressions {
 		int z = 32; 
 		boolean b = false; 
 		
-		System.out.println("REPLACE THIS STRING WITH YOUR BOOLEAN EXPRESSION");
+		System.out.println( (x < y) == b   );
 	}
 
 }
--- src/P3_Consecutive.java
+++ src/P3_Consecutive.java
@@ -4,6 +4,8 @@
 	Date: 
 */
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P3_Consecutive {
@@ -11,20 +13,26 @@ public class P3_Consecutive {
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		System.out.println("This program calculates if three input values are consecutive.");
-		
+		Arrays.sort(test);
+		for (int i = 0; i< test.length -1; i++) {
+			if (test[i] +1 != test [i + 1]) {
+				//Not sequential
+			
+			}
+		}
 		String doItAgainResponse = "";
 		
 		do {
-			System.out.print("Enter value 1: ");
+			System.out.print("7");
 			int num1 = console.nextInt();
-			System.out.print("Enter value 2: ");
+			System.out.print("6 ");
 			int num2 = console.nextInt();
-			System.out.print("Enter value 3: ");
+			System.out.print("8");
 			int num3 = console.nextInt();
 		
 			System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
-			
 
+			
 			
 			System.out.print("Would you like to do it again (enter 'n' to quit)? ");
 			doItAgainResponse = console.next();
